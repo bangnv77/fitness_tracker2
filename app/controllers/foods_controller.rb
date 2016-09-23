3379ca20f017b9dd111e8cb09a707d9e08265c03
Bang Nguyen <bangnv77@yahoo.com>
2016-09-22 17:10:55 -0700

    working on allowing users to add a food item to the database via AJAX

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,17 +8,21 @@ class FoodsController < ApplicationController
   end
 
   def create
+    @foods = Food.all
     @food = Food.new(food_params)
     if @food.save
       flash[:notice] = "Food added to the database!"
-      redirect_to user_path(current_user)
+      respond_to do |format|
+        format.html {redirect_to user_path(current_user)}
+        format.js
+      end
     else
       flash[:alert] = "There was a problem adding your food, please try again"
-      render 'new'
+      render new
     end
   end
 
-private
+  private
   def food_params
     params.require(:food).permit(:name, :calories_per_serving, :serving_size)
   end
